5d7cb13836255c6aa0c0694474e4b0caae2e7dba
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-06-10 00:08:56 -0400

    Import BrowserRouter and modify code to show components depending on route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import './App.css';
 import { FrontPageImages } from './components/FrontPageImages';
 import { NavList } from './components/NavList';
 import { Footer } from './components/Footer';
+import { AboutUs } from './components/AboutUs';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <NavList />
-      <FrontPageImages />
-      <Footer />
+      <BrowserRouter>
+        <NavList />
+        <Routes>
+          <Route path="/" element={
+            <FrontPageImages />
+          }
+          />
+          <Route path="/AboutUs" element={
+            <AboutUs />
+          }
+          />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+
     </div>
   );
 }
