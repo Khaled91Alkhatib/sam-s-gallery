fcdb297d999faeb6e325503419ac43b3960c53c7
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-06-10 00:11:56 -0400

    Import useNavigate and use it to navigate to different routes onClick(s)

--- src/components/NavList.js
+++ src/components/NavList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './styles/NavList.css';
 import logoImage from "../images/fashion-dress-icon.png";
 import { CustomerService } from './CustomerService';
@@ -10,22 +11,26 @@ import { faBagShopping, faPhone } from "@fortawesome/free-solid-svg-icons";
 library.add(faBagShopping, faPhone);
 
 export const NavList = () => {
+
+  const navigate = useNavigate();
+  const onClickLogo = () => {
+    navigate('/');
+  };
+  const onClickAboutUs = () => {
+    navigate('/AboutUs');
+  };
+
   return (
     <div className="main-nav-bar">
-
       <div className="image-and-name">
         <div>Sam's</div>
-        <img className='logo' src={logoImage} alt='Logo' />
+        <img className='logo' onClick={onClickLogo} src={logoImage} alt='Logo' />
         <div><strong>Gallery</strong></div>
       </div>
-
       <div className="buttons">
+        <button className='about-us' onClick={onClickAboutUs}>About Us</button>
         <OurCollection />
-
         <button><FontAwesomeIcon icon="fa-solid fa-bag-shopping" />&nbsp; Shopping Bag</button>
-
-        <button>Pending Purchases</button>
-
         <CustomerService />
       </div>
     </div>
