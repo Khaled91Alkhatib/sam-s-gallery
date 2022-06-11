068f72f27d0ff8abb1251565e6532a344e126799
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-06-11 12:49:55 -0400

    Add onClick function for hats to navigate to a different route

--- src/components/OurCollection.js
+++ src/components/OurCollection.js
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./styles/OurCollection.css";
 
 export const OurCollection = () => {
+  const navigate = useNavigate();
+  const onClickHats = () => {
+    navigate('/Hats');
+  };
 
   const [ourCollectionData, setOurCollectionData] = useState(false);
-  const onClick = () => {
+  const onClickOurCollection = () => {
     setOurCollectionData(current => !current);
   };
 
   return (
     <div>
-      <button onClick={onClick} className='our-collection-button'>Our Collection</button>
+      <button onClick={onClickOurCollection} className='our-collection-button'>Our Collection</button>
       {ourCollectionData && (
         <div className='inner-collection-buttons'>
-          <button className='items'>Hats</button>
+          <button onClick={onClickHats} className='items'>Hats</button>
           <button className='items'>Belts</button>
           <button className='items'>Dresses</button>
           <button className='items'>Shoes</button>
