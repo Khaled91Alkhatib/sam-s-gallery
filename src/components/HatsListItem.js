93145e9096d5e2a0d52d7561fcc2f0aade449e55
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-06-12 19:49:30 -0400

    Create new component and create code and functionality to hanle button clicks

---
+++ src/components/HatsListItem.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import "./styles/HatsListItem.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCirclePlus, faCircleMinus } from "@fortawesome/free-solid-svg-icons";
+library.add(faCirclePlus, faCircleMinus);
+
+
+export const HatsListItem = ({ backClick, clickedItem }) => {
+
+  const [counter, setCounter] = useState(0);
+  const onPlusClick = () => {
+    setCounter(counter + 1);
+  };
+  const onMinusClick = () => {
+    if (counter >= 1) {
+      setCounter(counter - 1);
+    }
+  };
+  return (
+    <div className='overlay-style'>
+      <div className='background'>
+        <div className='image-to-desc' key={clickedItem.id}>
+          <img className='hat-item' src={`/images/${clickedItem.image_path}`} alt="hat" />
+          <div className='data-to-button'>
+            <div>
+              <div className='single-item-name'>
+                {clickedItem.name}
+              </div>
+              ${clickedItem.price}
+              <br />
+              <br />
+              <div className='item-description'>
+                {clickedItem.description}
+              </div>
+            </div>
+            <div className='both-buttons'>
+              <button className='item-buttons' onClick={() => { backClick(false); }}>back</button>
+              <div>
+                <div className='plus-counter-minus'>
+                  <button className='plus-minus' onClick={onMinusClick}><FontAwesomeIcon icon="fa-solid fa-circle-minus" /></button>
+                  <div>{counter}</div>
+                  <button className='plus-minus' onClick={onPlusClick}><FontAwesomeIcon icon="fa-solid fa-circle-plus" /></button>
+                </div>
+                <button className='item-buttons' >Add to shopping bag</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
