1a0c3caa220916e721d3e7834d05db9009a541d7
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-06-12 19:48:11 -0400

    Rename file

---
+++ src/components/HatsList.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import "./styles/HatsList.css";
+import { HatsListItem } from './HatsListItem';
+
+
+export const HatsList = () => {
+  const [moreDetails, setMoreDetails] = useState(false);
+  const [hatslist, setHatsList] = useState([]);
+  const [clickedItem, setClickedItem] = useState();
+  const [itemId, setItemId] = useState();
+
+  useEffect(() => {
+    axios.get('http://localhost:3002/api/hats')
+      .then((response) => {
+        setHatsList(response.data);
+      });
+  }, []);
+
+  // console.log("clicked item", clickedItem)
+
+  return (
+    <div className='main-hats-list'>
+      {hatslist.map((val) => {
+        return <div className='hats-list' key={val.id}>
+          <img className='hat-images' src={`/images/${val.image_path}`} alt="hat" />
+          <div className='item-name'>{val.name}</div>
+          <div>${val.price}</div>
+          <button onClick={() => { setItemId(val.id); setMoreDetails(true); setClickedItem(val); }} className='detail-button'>More Details</button>
+          {moreDetails && clickedItem && itemId === val.id && <HatsListItem backClick={setMoreDetails} clickedItem={clickedItem} />}
+        </div>;
+      })}
+    </div>
+  );
+};
